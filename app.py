b7448d31644f5365cb6729a33aa2df298c513003
Muhammad Ikhwan Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2022-04-11 06:50:16 +0700

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
+import numpy as np
 import joblib
-import numpy
 
 app = Flask(__name__)
 
@@ -8,24 +8,39 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', BMI=0)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    Status_Gender, Height = [x for x in request.form.values()]
-    
-    data = []
-    
-    data.append(float(Status_Gender))
-    data.append(float(Height))
-    
-    # Open trained model
-    file = open("modelbmi.pkl","rb")
-    
-    # Load trained model
+     if request.method == "POST":
+        #get form data
+        Status_Gender = request.form.get('Status_Gender')
+        Height = request.form.get('Height')  
+        #call preprocessDataAndPredict and pass inputs
+        try:
+            prediction = preprocessDataAndPredict(Status_Gender, Height)
+            #pass prediction to template
+            return render_template('index.html', Weight=prediction, Status_Gender=Status_Gender, Height=Height)
+        except ValueError:
+            return "Please Enter valid values"
+        pass
+pass
+
+def preprocessDataAndPredict(Status_Gender, Height):
+    #keep all inputs in array
+    test_data = [Status_Gender, Height]
+    print(test_data)
+    #convert value data into numpy array
+    test_data = np.array(test_data)
+    #reshape array
+    test_data = test_data.reshape(1,-1)
+    print(test_data)
+    #open file
+    file = open("modelbmi.sav","rb")
+    #load trained model
     trained_model = joblib.load(file)
-    
-    prediction = trained_model.predict([data])
-    output = round(float(prediction[0], 2))
-    return render_template('index.html', Weight=output, Status_Gender=Status_Gender, Height=Height)
+    #predict
+    prediction = trained_model.predict(test_data)
+    return prediction
+pass
 
 if __name__ == '__name__':
     app.run(debug=True)
